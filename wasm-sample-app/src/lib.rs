77947dd644c43054bd92df3af58d57e9324d7939
David Radunz <david@vimtura.com>
2022-04-14 15:49:09 -0400

    Checking in before I try wasmi again.

--- wasm-sample-app/src/lib.rs
+++ wasm-sample-app/src/lib.rs
@@ -1,17 +1,63 @@
-//extern crate wee_alloc;
 extern crate wasm2_std;
 
 use wasm2_std::{Api, debug_print, Env, Extern, HandleResponse, Querier, StdResult, Storage};
 use wasm2_std::memory::{consume_region, Region};
 
-// Use `wee_alloc` as the global allocator.
-//#[global_allocator]
-//static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+use serde::{Deserialize, Serialize};
+
+#[cfg(target_arch = "wasm32")]
+mod wasm {
+    //use super::contract;
+    use wasm2_std::{debug_print, do_handle, ExternalApi, ExternalQuerier, ExternalStorage};
+
+    /*
+    #[no_mangle]
+    extern "C" fn init(env_ptr: u32, msg_ptr: u32) -> u32 {
+        do_init(
+            &contract::init::<ExternalStorage, ExternalApi, ExternalQuerier>,
+            env_ptr,
+            msg_ptr,
+        )
+    }
+     */
+
+    #[no_mangle]
+    extern "C" fn handle(env_ptr: u32, msg_ptr: u32) -> u32 {
+        debug_print!("testing: env {}, msg {}", env_ptr, msg_ptr);
+
+        do_handle(
+            &crate::handle::<ExternalStorage, ExternalApi, ExternalQuerier>,
+            env_ptr,
+            msg_ptr,
+        )
+    }
+
+    /*
+    #[no_mangle]
+    extern "C" fn query(msg_ptr: u32) -> u32 {
+        do_query(
+            &contract::query::<ExternalStorage, ExternalApi, ExternalQuerier>,
+            msg_ptr,
+        )
+    }
+     */
+
+    // Other C externs like cosmwasm_vm_version_1, allocate, deallocate are available
+    // automatically because we `use cosmwasm_std`.
+}
 
 #[no_mangle]
 pub extern "C" fn run_wasm(input_data_ptr: i32) -> i32 {
     let input_data: Vec<u8> = unsafe { consume_region(input_data_ptr as *mut Region) };
     let input_data_str =  String::from_utf8(input_data).unwrap();
+
+    let target: Option<String>  = Some("hello world".to_string());
+
+    let encoded: Vec<u8> = bincode2::serialize(&target).unwrap();
+
+    if encoded.len() > 0 {
+
+    }
 
     //let inputs = serde_json::from_slice(&input_data).unwrap();
     //let object = #name {};
@@ -31,8 +77,10 @@ pub extern "C" fn run_wasm(input_data_ptr: i32) -> i32 {
     //return 0 as i32
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+#[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
-
+    DoNothing {}
 }
 
 pub fn handle<S: Storage, A: Api, Q: Querier>(
@@ -40,6 +88,7 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
     env: Env,
     _msg: HandleMsg,
 ) -> StdResult<HandleResponse> {
+
     debug_print!("handle called by {}", env.message.sender);
 
     return Ok(HandleResponse::default())
