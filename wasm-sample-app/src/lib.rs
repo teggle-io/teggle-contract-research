0adb518963856390d4dfa3190bcde22874e57fbb
David Radunz <david@vimtura.com>
2022-04-15 16:51:40 -0400

    Added simulate

--- wasm-sample-app/src/lib.rs
+++ wasm-sample-app/src/lib.rs
@@ -1,9 +1,8 @@
 extern crate wasm2_std;
 
-use wasm2_std::{Api, debug_print, Env, Extern, HandleResponse, Querier, StdResult, Storage};
-use wasm2_std::memory::{consume_region, Region};
-
 use serde::{Deserialize, Serialize};
+use wasm2_std::{Api, debug_print, Env, Extern, HandleResponse, Querier, StdError, StdResult, Storage};
+use wasm2_storage::PrefixedStorage;
 
 #[cfg(target_arch = "wasm32")]
 mod wasm {
@@ -12,7 +11,7 @@ mod wasm {
 
     #[no_mangle]
     extern "C" fn handle(env_ptr: u32, msg_ptr: u32) -> u32 {
-        debug_print!("testing: env {}, msg {}", env_ptr, msg_ptr);
+        debug_print!("handle: env {}, msg {}", env_ptr, msg_ptr);
 
         do_handle(
             &crate::handle::<ExternalStorage, ExternalApi, ExternalQuerier>,
@@ -32,50 +31,50 @@ mod wasm {
      */
 }
 
-#[no_mangle]
-pub extern "C" fn run_wasm(input_data_ptr: i32) -> i32 {
-    let input_data: Vec<u8> = unsafe { consume_region(input_data_ptr as *mut Region) };
-    let input_data_str =  String::from_utf8(input_data).unwrap();
-
-    let target: Option<String>  = Some("hello world".to_string());
-
-    let encoded: Vec<u8> = bincode2::serialize(&target).unwrap();
-
-    if encoded.len() > 0 {
-
-    }
-
-    //let inputs = serde_json::from_slice(&input_data).unwrap();
-    //let object = #name {};
-    //let result = object.run(inputs);
-
-    //let return_data = serde_json::to_vec(&result).unwrap();
-
-
-    //unsafe { copy(return_data.as_ptr(), input_data_ptr as *mut u8, return_data.len()); }
-
-    //return_data.len() as i32
-
-    debug_print("Hello World");
-
-    return input_data_str.len() as i32;
-
-    //return 0 as i32
-}
-
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
     DoNothing {}
 }
 
+pub const PREFIX_SIM: &[u8] = b"sim";
+
+// For exact replica for test.
+use std::any::type_name;
+
+pub fn set_bin_data<T: Serialize, S: Storage>(
+    storage: &mut S,
+    key: &[u8],
+    data: &T,
+) -> StdResult<()> {
+    let bin_data =
+        bincode2::serialize(&data).map_err(|e| StdError::serialize_err(type_name::<T>(), e))?;
+    storage.set(key, &bin_data);
+    Ok(())
+}
+
 pub fn handle<S: Storage, A: Api, Q: Querier>(
-    _deps: &mut Extern<S, A, Q>,
+    deps: &mut Extern<S, A, Q>,
     env: Env,
     _msg: HandleMsg,
 ) -> StdResult<HandleResponse> {
-
     debug_print!("handle called by {}", env.message.sender);
 
-    return Ok(HandleResponse::default())
+    let owner = deps.api.canonical_address(&env.message.sender)?;
+
+    let count: i32 = 1000;
+    for seq in 0..count {
+        let mut sim_storage = PrefixedStorage::multilevel(
+            &[PREFIX_SIM, &owner.as_slice()],
+            &mut deps.storage,
+        );
+
+        // For 1:1 comparison with the native version.
+        //sim_storage.set(&[seq as u8], &[seq as u8]);
+
+        set_bin_data(&mut sim_storage, &seq.to_le_bytes(), &seq)?;
+    }
+
+
+    return Ok(HandleResponse::default());
 }
